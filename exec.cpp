f7305dd19b6c7bae465798efe0e3bc06f35fcaaa
DESKTOP-F74V2V6\qinyu <qinyufight123@126.com>
2021-12-28 19:29:20 +0800

    test：书写lambda表达式的测试用例

--- exec.cpp
+++ exec.cpp
@@ -1,21 +1,16 @@
-#include "dynet/dim.hpp"
+
 #include <vector>
-#include <string>
+#include <algorithm>
 #include <iostream>
 using namespace std;
-using namespace dynet;
 
-struct Person {
-    Person() : name(""), id() {}
-    const std::string name;
-    int id;
-};
 
-std::ostream& operator<<(std::ostream& os, Person& p) {
-    std::cout << p.name << std::endl;
-} 
 int main() {
-    std::vector<Person> p(10);
-    std::cout << p[2] <<endl;
+
+    int num = 123;
+    auto f = [&] { return num - 5; };
+    num = 100;
+    int res = f();
+    std::cout << res << std::endl;
     return 0;
 }
