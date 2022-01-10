e3725c822ef7895e080d4deea67b29ce3a55478d
qinyu <qinyufight123@126.com>
2022-01-10 22:59:14 +0800

    书写dynet的cmake，目前编译阶段还是缺少头文件

--- exec.cpp
+++ exec.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main() {
     float* data = new float[4]{0.,1.,2.,3};
-    const dynet::Dim d({4}, 1);
+    const dynet::Dim d({4}, 1); 
+    // FIXME: 目前需要把device_cpu做成库, 但是dim是不需要的是因为所有的实现是写在头文件里面的。
     dynet::Device_CPU* device = new dynet::Device_CPU(0, std::string("512"), false);
     dynet::Tensor t(d, data, device, dynet::DeviceMempool::FXS);
-    
 }
