f50de5c832c62fe8199440b4484cce9ca24905f7
shwild <qinyufight123@126.com>
2022-01-18 09:30:11 +0800

    验证不完整的struct dynet::Tensor类型是由于device.cpp/hpp没有找到tensor定义所导致的。

--- dynet/device-structs.hpp
+++ dynet/device-structs.hpp
@@ -1,6 +1,5 @@
 #ifndef DYNET_DEVICE_STRUCTS_HPP
 #define DYNET_DEVICE_STRUCTS_HPP
-
 namespace dynet {
 
 enum class DeviceType {CPU, GPU};
@@ -12,7 +11,7 @@ enum class DeviceType {CPU, GPU};
  * NONE  -> when a memory pool has not been assigned yet
  */
 enum class DeviceMempool {FXS = 0, DEDFS = 1, PS = 2, SCS = 3, NONE = 4};
-struct Tensor; 
+struct Tensor {}; 
 
 struct DeviceMempoolSizes {
     size_t used[4];
