3894558908d8e4d1da2a02da87d7aed27d162d5f
shwild <qinyufight123@126.com>
2022-02-08 18:32:32 +0800

    feat：添加某些函数模板

--- dynet/tensor-eigen.hpp
+++ dynet/tensor-eigen.hpp
@@ -8,23 +8,23 @@
 #include <unsupported/Eigen/CXX11/Tensor>
 
 namespace dynet {
-	
+    
 inline Eigen::Map<Eigen::MatrixXf> mat(Tensor& t) {
-	DYNET_ARG_CHECK((t.d.batch_elems() == 1 && t.d.ndims() < 3), 
-	"Attempted to access Tensor with more than one batch element or more than two dimensions in matrix form: " << t.d);
-	return Eigen::Map<Eigen::MatrixXf>(t.v, t.d.rows(), t.d.cols());
+    DYNET_ARG_CHECK((t.d.batch_elems() == 1 && t.d.ndims() < 3), 
+    "Attempted to access Tensor with more than one batch element or more than two dimensions in matrix form: " << t.d);
+    return Eigen::Map<Eigen::MatrixXf>(t.v, t.d.rows(), t.d.cols());
 }
 
 inline Eigen::Map<Eigen::MatrixXf> mat(const Tensor& t) {
-	return Eigen::Map<Eigen::MatrixXf>(t.v, t.d.rows(), t.d.cols());
+    return Eigen::Map<Eigen::MatrixXf>(t.v, t.d.rows(), t.d.cols());
 } // 这个函数是对上面的函数的重载
 
 inline Eigen::Map<Eigen::VectorXf> vec(Tensor& t) {
-	return Eigen::Map<Eigen::VectorXf>(t.v, t.d.size());
+    return Eigen::Map<Eigen::VectorXf>(t.v, t.d.size());
 }
 
 inline const Eigen::Map<Eigen::VectorXf> vec(const Tensor & t) {
-	return Eigen::Map<Eigen::VectorXf>(t.v, t.d.size());
+    return Eigen::Map<Eigen::VectorXf>(t.v, t.d.size());
 }
 
 /**
@@ -34,15 +34,15 @@ inline const Eigen::Map<Eigen::VectorXf> vec(const Tensor & t) {
  */
 
 inline Eigen::TensorMap<Eigen::Tensor<float, 1>> tvec(Tensor& t) {
-	return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, t.d.size());
+    return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, t.d.size());
 }
 
 inline const Eigen::TensorMap<Eigen::Tensor<float, 1>> tvec(const Tensor & t) {
-  	return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, t.d.size());
+      return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, t.d.size());
 }
 
 inline Eigen::TensorMap<Eigen::Tensor<float, 2>> tbvec(Tensor& t) {
-	return Eigen::TensorMap<Eigen::Tensor<float, 2>>(t.v, t.d.batch_size(), t.d.batch_elems());
+    return Eigen::TensorMap<Eigen::Tensor<float, 2>>(t.v, t.d.batch_size(), t.d.batch_elems());
 }
 
 inline const Eigen::TensorMap<Eigen::Tensor<float, 2>> tbvec(const Tensor & t) {
@@ -54,11 +54,39 @@ template <int Order> inline Eigen::TensorMap<Eigen::Tensor<float, Order>> t(Tens
 // 声明一个重载函数, 重载上面的函数, 注意函数重载返回值不能作为判断条件。这里发生重载主要是参数的类型不同
 template <int Order> inline const Eigen::TensorMap<Eigen::Tensor<float, Order>> t(const Tensor & t); 
 
-// 对模板的一次全特化扩充。
+// 对模板的一次全特化扩充, 模板实参是0。
 template <> inline Eigen::TensorMap<Eigen::Tensor<float, 0>> t<0>(Tensor& t) {
-	DYNET_ASSERT(t.d.batch_elems() == 1 && t.d.size() == 1, "Illegal access of tensor in function t<0>(Tensor & t): dim=" << t.d);
-	return Eigen::TensorMap<Eigen::Tensor<float, 0>>(t.v);
+    DYNET_ASSERT(t.d.batch_elems() == 1 && t.d.size() == 1, "Illegal access of tensor in function t<0>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 0>>(t.v);
 }
+
+template<> inline const Eigen::TensorMap<Eigen::Tensor<float, 0>> t<0>(const Tensor & t) {
+    DYNET_ASSERT(t.d.batch_elems() == 1 && t.d.size() == 1, "Illegal access of tensor in function t<0>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 0>>(t.v);
+}
+
+
+template<> inline Eigen::TensorMap<Eigen::Tensor<float, 1>> t<1>(Tensor & t) {
+    DYNET_ASSERT(t.d.batch_elems() == 1 && (t.d.ndims() == 1 || t.d.size() == t.d.rows()), "Illegal access of tensor in function t<1>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, (int)t.d[0]);
+}
+
+
+template<> inline const Eigen::TensorMap<Eigen::Tensor<float, 1>> t<1>(const Tensor & t) {
+    DYNET_ASSERT(t.d.batch_elems() == 1 && (t.d.ndims() == 1 || t.d.size() == t.d.rows()), "Illegal access of tensor in function t<1>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, (int)t.d[0]);
+}
+
+template <> inline Eigen::TensorMap<Eigen::Tensor<float, 1>>t<1>(Tensor& t) {
+    DYNET_ASSERT(t.d.batch_elems() == 1 && (t.d.ndims() == 1 || t.d.size() == t.d.rows()), "Illegal access of tensor in function t<1>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, (int)t.d[0]);	
+}
+
+template<> inline const Eigen::TensorMap<Eigen::Tensor<float, 1>> t<1>(const Tensor & t) {
+    DYNET_ASSERT(t.d.batch_elems() == 1 && (t.d.ndims() == 1 || t.d.size() == t.d.rows()), "Illegal access of tensor in function t<1>(Tensor & t): dim=" << t.d);
+    return Eigen::TensorMap<Eigen::Tensor<float, 1>>(t.v, (int)t.d[0]);
+}
+
 
 }
 #endif
